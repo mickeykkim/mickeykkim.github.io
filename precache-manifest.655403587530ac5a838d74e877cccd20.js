a93443329137c902382e5c068d57dc0ec05e67a1
Mickey K <mickeykkim@gmail.com>
2020-01-14 11:31:32 +0000

    Updates

---
+++ precache-manifest.655403587530ac5a838d74e877cccd20.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "764bee423de931af25eed04b3c833139",
+    "url": "/index.html"
+  },
+  {
+    "revision": "648d7c3335e8b8c5d744",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "fa0d126b0c7c074fda6f",
+    "url": "/static/js/2.7c3c03ec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.7c3c03ec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "648d7c3335e8b8c5d744",
+    "url": "/static/js/main.740e7c3c.chunk.js"
+  },
+  {
+    "revision": "a65c89998f3c310da705",
+    "url": "/static/js/runtime-main.877d2b80.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
