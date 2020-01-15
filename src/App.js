437b05e0b8ab498feac82dbd8e7ebb3a630da4e5
Mickey K <mickeykkim@gmail.com>
2020-01-15 11:03:23 +0000

    updated fontawesome icons, ajusted publications css

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Footer from './Components/Footer';
 import About from './Components/About';
 import Resume from './Components/Resume';
 import Contact from './Components/Contact';
-import Testimonials from './Components/Testimonials';
+import Publications from './Components/Publications';
 import Portfolio from './Components/Portfolio';
 
 class App extends Component {
@@ -15,7 +15,6 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      //foo: 'bar',
       resumeData: {}
     };
 
@@ -50,7 +49,7 @@ class App extends Component {
         <About data={this.state.resumeData.main}/>
         <Resume data={this.state.resumeData.resume}/>
         <Portfolio data={this.state.resumeData.portfolio}/>
-        <Testimonials data={this.state.resumeData.testimonials}/>
+        <Publications data={this.state.resumeData.publications}/>
         <Contact data={this.state.resumeData.main}/>
         <Footer data={this.state.resumeData.main}/>
       </div>
